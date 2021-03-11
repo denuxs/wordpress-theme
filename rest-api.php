bce86ffb021af2ba413cea67939deec976c1d530
Denis Lopez <denis@legger.co>
2021-03-10 21:31:43 -0600

    changes rest api

--- rest-api.php
+++ rest-api.php
@@ -1,23 +1,68 @@
 <?php
 
-// Get all posts and custom fields
-function blog_get_posts( $params ) {
-  $posts =  get_posts();
+define("API_URL", "api/v1");
 
-  $data = [];
-  foreach($posts as $post ) {
-    $data["post_title"] = $post->post_title;
-    $data['custom_fields'] = get_post_meta($post->ID);
-  }
+$filters_default = [
+    'sort_order'  => 'desc',
+    'post_type'   => 'page',
+    'post_status' => 'publish',
+];
 
-  return $data;
-  // return new WP_REST_Response('Howdy!!');
+// Get all pages and custom fields
+// localhost:8000/wp-json/api/v1/pages
+function blog_get_pages()
+{
+
+    $pages = get_pages($filters_default);
+    return get_custom_post($pages);
 }
 
-add_action('rest_api_init', function() {
-  register_rest_route( 'blog/v1', '/posts', [
-    'methods' => 'GET',
-    'callback' => 'blog_get_posts',
-    'permission_callback' => '__return_true',
-  ] );
-} );
+// Get page by slug and custom types
+// localhost:8000/wp-json/api/v1/pages/home
+function blog_get_page(WP_REST_Request $request)
+{
+
+    $param = $request->get_url_params();
+
+    $args = [
+        'name'        => $param['slug'],
+        'post_type'   => 'page',
+        'post_status' => 'publish',
+    ];
+
+    $pages = get_posts($args);
+    return get_custom_post($pages);
+}
+
+// Extract Post Fields
+function get_custom_post($pages)
+{
+    $data = [];
+    foreach ($pages as &$page) {
+        $data["ID"]          = $page->ID;
+        $data['post_status'] = $page->post_status;
+        $data['post_author'] = $page->post_author;
+        $data['post_type']   = $page->post_type;
+        $data['post_title']  = $page->post_title;
+
+        // get custom fields
+        $custom_fields         = get_fields($page->ID);
+        $data['custom_fields'] = $custom_fields;
+    }
+
+    return $data;
+}
+
+add_action('rest_api_init', function () {
+
+    register_rest_route(API_URL, '/pages', [
+        'methods'  => 'GET',
+        'callback' => 'blog_get_pages',
+    ]);
+
+    register_rest_route(API_URL, '/pages/(?P<slug>[a-zA-Z0-9-]+)', array(
+        'methods'  => 'GET',
+        'callback' => 'blog_get_page',
+    ));
+
+});
